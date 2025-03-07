83cc8862bd5e7c1cf93b8b6a53876d84e0449fe9
Samarth Vohra <officialdevelopersamarth@gmail.com>
2025-03-07 22:21:57 +0530

    lecture 6 codes

---
+++ Lecture-6/hoisting-cont/app.js
@@ -0,0 +1,56 @@
+
+// hoisting
+// console.log(a);
+// abc();
+
+// var a = 10;
+// function abc(){
+//     console.log("hello");
+// }
+
+
+// -------------------------------
+
+
+// console.log(b);
+// abc();
+
+
+// function abc(){
+//     console.log("hello");
+// }
+
+// let a = 10;
+// const c = 30;
+// var b = 20
+
+// -------------------------------
+
+
+// console.log(b);
+// function vari(){
+
+// }
+// var a = 10;
+// let b = 100;
+// vari()
+// console.log(a);
+
+// -------------------------------
+
+// const b = 100;
+// console.log(b)
+// calll()
+// function calll(){
+//     console.log("lllolll");
+// }
+
+
+
+
+
+
+
+
+
+
---
+++ Lecture-6/scopes/app.js
@@ -0,0 +1,122 @@
+
+
+
+// var a = 10;
+
+// function bhai(){
+//     console.log("hello");
+//     console.log(a);
+// }
+
+// bhai()
+
+
+// -----------------------------
+
+// var a = 10;   // global
+// let b = 20;   // script
+// const c = 30; // script
+
+// if(true){
+//     var d = 100;   // global
+//     let e = 200;   // block
+//     const f = 300; // block
+// }
+
+// function sam(){
+//     var g = 1000;   // functional
+//     let h = 2000;   // block
+//     const i = 3000; // block
+// }
+// sam()
+
+
+
+
+// -----------------------------
+
+
+
+// function sam(){
+//     console.log(a);
+//     console.log(b);
+    
+//     var a = 10;
+//     let b = 20;
+// }
+
+// sam()
+
+// -----------------------------
+
+
+// var a = 100
+// function sam(){
+//     console.log(a);
+//     console.log(b);
+//     var b = 300;
+//     var a = 400;
+// }
+
+// console.log(a);
+// sam()
+// console.log(a);
+
+
+
+// // ------------------------
+
+// var a = 100;
+
+// if(true){
+//     console.log(a);
+//     let b = 300;
+//     console.log(b);
+//     var a = 400;
+// }
+
+// console.log(a);
+// console.log(b);
+
+
+// ------------------------
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// ------------------------
+
+
+var a = 10;
+let b = 20;
+const c = 30;
+console.log(a); console.log(b); console.log(c);
+
+if(true){
+    var a = 100;
+    let b = 200;
+    const c = 300;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+console.log(a); console.log(b); console.log(c);
+function sam(){
+    var a = 1000;
+    let b = 2000;
+    const c = 3000;
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+sam();
+console.log(a); console.log(b); console.log(c);
+
+
+
+
+
